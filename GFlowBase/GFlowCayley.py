Incorrect FIOR computation corrected

GFlowBase/GFlowCayley.py
@@ -115,8 +115,10 @@ class GFlowCayleyLinear(tf.keras.Model):
         Fout = tf.reshape(
             tf.reduce_sum(F,axis=1),
             shape=(self.batch_size*self.batch_memory,self.path_length,1)
-        ) + tf.reshape(self.paths_reward,shape=(self.batch_size*self.batch_memory,self.path_length,1))
-        Fin = tf.ones_like(Fout)*self.initial_flow
+        )
+        R = tf.reshape(self.paths_reward,shape=(self.batch_size*self.batch_memory,self.path_length,1))
+        Finit = tf.ones_like(Fout)*self.initial_flow
+        Fin = tf.zeros_like(Fout)
         for i in tf.range(0, nactions, 1):
             Fin += tf.reshape(
                 self.FlowEstimator(
@@ -127,7 +129,7 @@ class GFlowCayleyLinear(tf.keras.Model):
                 )[:,i],
                 shape=(self.batch_size*self.batch_memory,self.path_length,1)
             )
-        Flow = tf.concat([Fin,Fout],axis=-1)
+        Flow = tf.concat([Fin,Fout,Finit, R],axis=-1)
         return Flow # (batch_size*path_length,2)
 
     @tf.function
@@ -172,24 +174,14 @@ class GFlowCayleyLinear(tf.keras.Model):
     @tf.function
     def FinOutReward_foo(self):
         batch_size,length = self.paths.shape[:2]
-        FinFout = tf.reshape(
-            self(tf.reshape(self.paths, shape=(-1,self.embedding_dim))),
-            shape=(batch_size,length,2)
-        )
-        # R = tf.reshape(self.reward(tf.reshape(paths,shape=(-1,self.embedding_dim))),shape=(batch_size,length,1))
-        R = tf.reshape(self.paths_reward,shape=(batch_size,length,1))
-        return tf.concat([FinFout,R],axis=-1)
+        FinFoutInitR = self(tf.reshape(self.paths, shape=(-1,self.embedding_dim)))
+        return tf.concat([FinFoutInitR[:,:,:2],FinFoutInitR[:,:,-1:]],axis=-1)
 
     @tf.function
     def FinOutReward(self):
         batch_size,length = self.paths.shape[:2]
-        FinFout = tf.reshape(
-            self(tf.reshape(self.paths, shape=(-1,self.embedding_dim))),
-            shape=(batch_size,length,2)
-        )
-        # R = tf.reshape(self.reward(tf.reshape(paths,shape=(-1,self.embedding_dim))),shape=(batch_size,length,1))
-        R = tf.reshape(self.paths_reward,shape=(batch_size,length,1))
-        self.FIOR.assign(tf.concat([FinFout,R],axis=-1))
+        FinFoutInitR = self(tf.reshape(self.paths, shape=(-1,self.embedding_dim)))
+        self.FIOR.assign(tf.concat([FinFoutInitR[:,:,:2],FinFoutInitR[:,:,-1:]],axis=-1))
 
 
     @tf.function
@@ -199,8 +191,7 @@ class GFlowCayleyLinear(tf.keras.Model):
         delta = 1.
         with tf.GradientTape() as tape:
             Flow = self(tf.reshape(self.paths,shape=(-1,self.embedding_dim)))
-            Flow = tf.reshape(Flow,shape=(-1,2))
-            nu = tf.reshape(self.density,shape=(-1,))
+            nu = self.density
             loss = self.compiled_loss(Flow, nu, regularization_losses=self.losses)
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)

GFlowBase/kernel.py
@@ -2,9 +2,9 @@
 import tensorflow as tf
 import numpy as np
 
-class total(tf.keras.layers.Layer):
+class softmax_head(tf.keras.layers.Layer):
     def __init__(self,kernel):
-        super(total, self).__init__()
+        super(softmax_head, self).__init__()
         self.kernel = kernel
 
     def build(self, input_shape):
@@ -16,6 +16,22 @@ class total(tf.keras.layers.Layer):
     def call(self, inputs):
         V = self.kernel(inputs)
         return tf.math.exp(V[:,-1:])*tf.nn.softmax(V[:,:-1])
+
+class direct_head(tf.keras.layers.Layer):
+    def __init__(self,kernel):
+        super(direct_head, self).__init__()
+        self.kernel = kernel
+
+    def build(self, input_shape):
+        self.kernel.build(input_shape)
+
+    def load_weights(self,*args,**kwargs):
+        self.kernel.load_weights(*args,**kwargs)
+
+    def call(self, inputs):
+        V = self.kernel(inputs)
+        return tf.math.abs(V)
+        # return tf.math.abs(V[:,-1:])*tf.nn.softmax(V[:,:-1])
         # return tf.math.abs(V[:,-1:])*tf.nn.softmax(V[:,:-1])
 
 class PositionalEncoding(tf.keras.layers.Layer):
@@ -34,7 +50,7 @@ class PositionalEncoding(tf.keras.layers.Layer):
         axis=-1
     )
 
-def dense_gen(ndirectactions,kernel_depth=2,width=64,final_activation=tf.math.abs,softmax_head=True,kernel_options={},encoding=-1):
+def dense_gen(ndirectactions,kernel_depth=2,width=64,final_activation=tf.math.abs,head=softmax_head,kernel_options={},encoding=-1):
     flow_kernel = tf.keras.Sequential(name='flow_kernel')
     if encoding>0:
         flow_kernel.add(PositionalEncoding(encoding))
@@ -42,12 +58,12 @@ def dense_gen(ndirectactions,kernel_depth=2,width=64,final_activation=tf.math.ab
         flow_kernel.add(
             tf.keras.layers.Dense(width,**kernel_options)
         )
-    if softmax_head:
+    if head is softmax_head:
         nout= ndirectactions + 1
-        head = total
+        head = softmax_head
     else:
         nout = ndirectactions
-        head = lambda x:x
+        head = direct_head
     flow_kernel.add(
         tf.keras.layers.Dense(
             nout,

GFlowBase/losses.py
@@ -78,6 +78,8 @@ class MeanABError(tf.keras.losses.Loss):
 
     @tf.function
     def call(self, Flow, nu):
-        Fout = Flow[:, 1]
-        Fin = Flow[:, 0]
-        return tf.reduce_sum(nu*self.A(Fout-Fin)*self.B(Fin, Fout))
+        Finstar = Flow[:,:, 0]
+        Foutstar = Flow[:,:, 1]
+        Finit = Flow[:,:, 2]
+        R =  Flow[:,:, 3]
+        return tf.reduce_mean(tf.reduce_sum(nu*self.A(Foutstar+R-Finstar-Finit)*self.B(Finstar, Foutstar),axis=-1))

mainv2.py
@@ -126,10 +126,12 @@ FlowEstimator_options = {
             'width' : MLP_WIDTH,
             # 'final_activation' : tf.keras.layers.LeakyReLU(),
             'final_activation' : 'linear',
+            # 'final_activation' : tf.math.abs,
             # 'final_activation' : 'sigmoid',
             # 'final_activation' : 'swish',
             # 'final_activation' : lambda x: 5*tf.math.tanh(x),
             'encoding':ENCODING,
+            # 'softmax_head': False
         },
         'kernel_options': {
             'activation': tf.keras.layers.LeakyReLU(),
@@ -223,7 +225,7 @@ Scheduler = [
 loss_fn_dic = {
     'powB': lambda alphaA,alphaB,betaB:MeanABError(A=Apower(alphaA),B=Bpower(alphaB,betaB)),
     'AlogsquareB': lambda alphaA,alphaB,betaB:MeanABError(A=Alogsquare(alphaA),B=Bpower(alphaB,betaB)),
-    'AlogsquareBdelta': lambda alphaA,alphaB,betaB,deltaB:MeanABError(A=Alogsquare(alphaA),B=Bpower(alpha=alphaB,beta=betaB,delta=delta)),
+    'AlogsquareBdelta': lambda alphaA,alphaB,betaB,deltaB:MeanABError(A=Alogsquare(alphaA),B=Bpower(alpha=alphaB,beta=betaB,delta=deltaB)),
     'pow': lambda alphaA:MeanABError(A=Apower(alphaA)),
     'Bengio':lambda :divergence(),
 }
@@ -231,6 +233,8 @@ key = LOSS.split(',')[0]
 param = [eval(x) for x in LOSS.split(',')[1:]]
 flow.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
+    # optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
+    # optimizer=tf.keras.optimizers.SGD(learning_rate=LR),
     loss=loss_fn_dic[key](*param)
 )
 