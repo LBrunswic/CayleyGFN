4d97a204ea8f9e083bfe36689c793a9da9bbc895
maxbrain <poker@brunswic.fr>
2023-11-22 13:50:12 +0800

    hyperparamters new version

--- numa_profile.py
+++ numa_profile.py
@@ -8,7 +8,7 @@ results = np.zeros((N_CPU,N_ATTEMPTS))
 for cpu in range(N_CPU):
     for attempt in range(N_ATTEMPTS):
         T = time.time()
-        os.system(f'numactl --physcpubind={cpu} python3 docker_main.py profile 2345 > cpu_{cpu}.log')
+        os.system(f'numactl --physcpubind={cpu} python3 docker_main.py > cpu_{cpu}.log')
         results[cpu,attempt] = time.time()-T
         with open(RESULTS_FILE,'wb') as f:
             np.save(f,results)
