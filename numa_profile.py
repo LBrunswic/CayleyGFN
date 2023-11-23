04229a512756ab9169621fe84fa5f551f5c53205
maxbrain <poker@brunswic.fr>
2023-11-23 13:31:59 +0800

    numaprofiling

--- numa_profile.py
+++ numa_profile.py
@@ -15,20 +15,14 @@ parser.add_argument(
     default=32,
     help='Number of experiments Repeat',
 )
-parser.add_argument(
-    '--memory',
-    type=int,
-    default=-1,
-    help='Non positive value activate memory growth. Positive values set the actual GPU memory limit in MB'
-)
 args = parser.parse_args().__dict__
 
 N_ATTEMPTS = 2
 N_CPU = 32
+RESULTS_FILE = os.path.join('RESULTS','numa_profile_%s.npy' % time.time())
+results = np.zeros((N_CPU,N_ATTEMPTS))
 for attempt in range(N_ATTEMPTS):
     for PACK in range(1,9):
-        RESULTS_FILE = os.path.join('RESULTS','numa_profile_%s.npy' % time.time())
-        results = np.zeros((N_CPU,N_ATTEMPTS))
         for cpu in range(8,N_CPU-PACK):
             cpurange=f'{cpu}-{cpu+PACK-1}'
             T = time.time()
