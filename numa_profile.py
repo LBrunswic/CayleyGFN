609de483abc114586813310cf534c6055b201edf
maxbrain <poker@brunswic.fr>
2023-11-23 13:29:18 +0800

    numaprofiling

--- numa_profile.py
+++ numa_profile.py
@@ -1,17 +1,41 @@
 import os
 import time
 import numpy as np
-N_ATTEMPTS = 1
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='numaprofile_CayleyGFN',
+                    description='Test cpu affinity'
+)
+
+
+parser.add_argument(
+    '--N_ATTEMPTS',
+    type=int,
+    default=32,
+    help='Number of experiments Repeat',
+)
+parser.add_argument(
+    '--memory',
+    type=int,
+    default=-1,
+    help='Non positive value activate memory growth. Positive values set the actual GPU memory limit in MB'
+)
+args = parser.parse_args().__dict__
+
+N_ATTEMPTS = 2
 N_CPU = 32
-RESULTS_FILE = os.path.join('RESULTS','numa_profile_%s.npy' % time.time())
-results = np.zeros((N_CPU,N_ATTEMPTS))
-for cpu in range(N_CPU):
-    for attempt in range(N_ATTEMPTS):
-        T = time.time()
-        os.system(f'numactl --physcpubind={cpu} python3 docker_main.py > cpu_{cpu}.log')
-        results[cpu,attempt] = time.time()-T
-        with open(RESULTS_FILE,'wb') as f:
-            np.save(f,results)
+for attempt in range(N_ATTEMPTS):
+    for PACK in range(1,9):
+        RESULTS_FILE = os.path.join('RESULTS','numa_profile_%s.npy' % time.time())
+        results = np.zeros((N_CPU,N_ATTEMPTS))
+        for cpu in range(8,N_CPU-PACK):
+            cpurange=f'{cpu}-{cpu+PACK-1}'
+            T = time.time()
+            os.system(f'numactl --physcpubind={cpurange} python3 docker_main.py > cpu_{cpurange}.log')
+            results[PACK-1,cpu,attempt] = time.time()-T
+            with open(RESULTS_FILE,'wb') as f:
+                np.save(f,results)
 
 
 
