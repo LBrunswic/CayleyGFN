c2a732de0b32d7ffdb5213f369eb5373cf320d28
maxbrain <poker@brunswic.fr>
2023-11-24 12:28:36 +0800

    numaprofiling debug

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 docker build --no-cache --tag ploptest - < Dockerfile
 #docker run --rm --gpus '"device=0"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 numa_profile.py
 #docker run --rm --gpus '"device=1"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 numa_profile.py
-docker run --rm --gpus '"device=0"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  numactl --physcpubind=0-3 "python3 launch.py --HP_FOLDER='Hyperparameters/experimental_settings' --pool_size='48'" & disown
-docker run --rm --gpus '"device=1"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  numactl --physcpubind=4-7 "python3 launch.py --HP_FOLDER='Hyperparameters/experimental_settings_low' --pool_size='16'"" & disown
+docker run --rm --gpus '"device=0"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=0-3 --HP_FOLDER='Hyperparameters/experimental_settings' --pool_size='48' & disown
+docker run --rm --gpus '"device=1"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=4-7 --HP_FOLDER='Hyperparameters/experimental_settings_low' --pool_size='16' & disown
 
 
 
