3793489b7163c6d8c20ccb645b0c1ea5ec650a38
maxbrain <poker@brunswic.fr>
2023-11-24 12:17:32 +0800

    numaprofiling debug

--- launch.py
+++ launch.py
@@ -11,9 +11,15 @@ parser.add_argument(
     default=FOLDER,
     help=f'Provide the experimental setting file, If not provided, defaults to {FOLDER}'
 )
+parser.add_argument(
+    '--pool_size',
+    type=str,
+    default=32,
+    help=f'Set the experiment pool size, the effect of results is small ans is simply intended to improve efficiency'
+)
 
 args = parser.parse_args().__dict__
-FOLDER = args['HP_FOLDER']
+
 
 for hp_file_name in os.listdir(FOLDER):
     hp_file_path = os.path.join(FOLDER,hp_file_name)
@@ -21,5 +27,5 @@ for hp_file_name in os.listdir(FOLDER):
     data_save = os.path.join('RESULTS',hash + '.csv')
     if not os.path.exists(data_save):
         print(f'Launching {hp_file_path}')
-        os.system(f'python3 docker_main.py --pool_size=32 --hp_file={hp_file_path}')
+        os.system(f"python3 docker_main.py --pool_size={args['pool_size']} --hp_file={args['HP_FOLDER']}")
 
