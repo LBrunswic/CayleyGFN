2a5b197993dd60fad238e48d4eba7bc71437249b
maxbrain <poker@brunswic.fr>
2023-11-25 09:54:15 +0800

    numaprofiling debug

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ docker build --no-cache --tag ploptest - < Dockerfile
 #docker run --rm --gpus '"device=0"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 numa_profile.py
 #docker run --rm --gpus '"device=1"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 numa_profile.py
 docker run --rm --gpus '"device=0"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=0-2 --HP_FOLDER='Hyperparameters/experimental_settings' --pool_size='48' & disown
-#docker run --rm --gpus '"device=1"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=3-5 --HP_FOLDER='Hyperparameters/experimental_settings_low' --pool_size='16' & disown
-#docker run --rm -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=6-15 --HP_FOLDER='Hyperparameters/experimental_settings_low_cpu' --pool_size='16' & disown
+docker run --rm --gpus '"device=1"' -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=3-5 --HP_FOLDER='Hyperparameters/experimental_settings_low' --pool_size='16' & disown
+docker run --rm -v /home/maxbrain/DATA/TaskForce/logs:/TASK/LOGS/ -v /home/maxbrain/DATA/TaskForce/Results:/TASK/RESULTS/ -v /home/maxbrain/DATA/TaskForce/Models:/TASK/MODELS/ ploptest  python3 launch.py --numactl=6-22 --HP_FOLDER='Hyperparameters/experimental_settings_low_cpu' --pool_size='16' & disown
 
 
 
